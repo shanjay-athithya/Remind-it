83d9017cff7594e7ebbdaab2bf7842a48932041d
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-08-30 18:55:37 +0530

    c3

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,6 +1,6 @@
-"use client";
 
-import { deleteDoc } from 'firebase/firestore';
+
+
 import React, { useState } from 'react';
 
 export default function TaskList({ tasks, onEdit, onDelete, filter }) {
